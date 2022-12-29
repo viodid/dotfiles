3bb0dd2618804fbbac02844eeae51b395cd4a972
viodid <david.zwsse@simplelogin.co>
2022-12-29 12:20:35 +0100

    change tmux and nvim colors

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -5,6 +5,7 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+    vim.cmd([[highlight ColorColumn guibg=#39506d]])
 end
 
 ColorMyPencils()
