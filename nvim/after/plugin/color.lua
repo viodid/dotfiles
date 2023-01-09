49cf3ab077b461fce12ce757adf6849a07c6bd63
viodid <david.zwsse@simplelogin.co>
2023-01-09 22:24:18 +0100

    config changed

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -5,7 +5,7 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
-    vim.cmd([[highlight ColorColumn guibg=#39506d]])
+    vim.cmd([[highlight ColorColumn guibg=#454545 ]])
 end
 
 ColorMyPencils()
